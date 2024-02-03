8e670348c4d316e8ec138665351ee3bc14accc68
vherasymenko <manqarium@gmail.com>
2024-02-03 14:25:08 +0200

    [Feature] add locale/date/currency formatter

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import '@fontsource/inter/400.css'
 import '@fontsource/inter/500.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/700.css'
-
+import moment from 'moment'
+import 'moment/locale/uk'
 import { CssBaseline, ThemeProvider } from '@mui/material'
 
 import App from './App'
 import { theme } from './theme'
+
+moment.locale('uk')
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
