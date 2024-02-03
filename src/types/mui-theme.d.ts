fe18ce32bc3c6e0ebeae45de08772ecb1729a7f6
vherasymenko <manqarium@gmail.com>
2024-02-03 18:16:10 +0200

    [Feature] extends mui types

---
+++ src/types/mui-theme.d.ts
@@ -0,0 +1,115 @@
+import '@mui/material/styles'
+
+declare module '@mui/material/styles/createPalette' {}
+
+declare module '@mui/material' {
+  interface Color {
+    25?: string
+  }
+}
+
+declare module '@mui/material/styles' {
+  interface PaletteColor {
+    400?: string
+    500?: string
+    600?: string
+    700?: string
+    800?: string
+    medium?: string
+  }
+
+  interface SimplePaletteColorOptions {
+    400?: string
+    500?: string
+    600?: string
+    700?: string
+    800?: string
+    medium?: string
+  }
+
+  interface Palette {
+    common: {
+      black: string
+      white: string
+    }
+    success: {
+      main: string
+      dark: string
+      light: string
+    }
+    warning: {
+      main: string
+      dark: string
+      light: string
+    }
+    error: {
+      main: string
+      dark: string
+      light: string
+    }
+    info: {
+      main: string
+      dark: string
+      light: string
+    }
+  }
+
+  interface PaletteOptions {
+    common: {
+      black: string
+      white: string
+    }
+    success: {
+      main: string
+      dark: string
+      light: string
+    }
+    warning: {
+      main: string
+      dark: string
+      light: string
+    }
+    error: {
+      main: string
+      dark: string
+      light: string
+    }
+    info: {
+      main: string
+      dark: string
+      light: string
+    }
+  }
+
+  interface TypographyVariants {
+    subtitle3: React.CSSProperties
+    subtitle4: React.CSSProperties
+    body3: React.CSSProperties
+    body4: React.CSSProperties
+    body5: React.CSSProperties
+    button1: React.CSSProperties
+    button2: React.CSSProperties
+  }
+
+  interface TypographyVariantsOptions {
+    subtitle3?: React.CSSProperties
+    subtitle4?: React.CSSProperties
+    body3?: React.CSSProperties
+    body4?: React.CSSProperties
+    body5?: React.CSSProperties
+    button1?: React.CSSProperties
+    button2?: React.CSSProperties
+  }
+}
+
+declare module '@mui/material/Typography' {
+  interface TypographyPropsVariantOverrides {
+    subtitle3: true
+    subtitle4: true
+    body3: true
+    body4: true
+    body5: true
+    button1: true
+    button2: true
+  }
+}
