b18ac87897d50ad990f533ae9a5b469e0a3e1705
vherasymenko <manqarium@gmail.com>
2024-02-03 13:58:18 +0200

    [Feature] add api

---
+++ src/api.ts
@@ -0,0 +1,14 @@
+import { JarModel } from './models/jar'
+
+export const getJarsList = async () => {
+  let jarsList = [] as JarModel[]
+
+  try {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/jars`)
+    jarsList = await res.json()
+  } catch (e) {
+    console.error(e)
+  }
+
+  return jarsList
+}
---
+++ src/models/jar.ts
@@ -0,0 +1,15 @@
+export interface JarModel {
+  _id: string
+  jarId: string
+  name: string
+  title: string
+  description: string
+  text: string
+  jarAmount: number
+  jarGoal: number
+  isClosed: boolean
+  username: string
+  messageLink: string
+  date: number
+  lastChangeTimestamp: string
+}
